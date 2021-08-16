215cf26b29af9e169f9cde1ea4c7e6b053a08a4d
Sachin <sachin.guruswamy@gmail.com>
2021-08-16 12:10:20 -0700

    Added turtlebot urdf with oak-d launcher example

---
+++ dai_turtlebot3_description/launch/turtlebot_urdf.launch.py
@@ -0,0 +1,26 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration, Command
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    bringup_dir = get_package_share_directory('dai_turtlebot3_description')
+    xacro_path = os.path.join(bringup_dir, 'urdf', 'turtlebot3_waffle_pi.urdf.xacro')
+    # urdf = open(urdf_path).read()
+    # doc = xacro.process_file(urdf_path, mappings={'simulate_obstacles' : 'false'})
+    print(xacro_path)
+
+    rsp_node = Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='tb_wafflle_description',
+            parameters=[{'robot_description': Command(
+                ['xacro', ' ', xacro_path])}])
+    
+    ld = LaunchDescription()
+
+    ld.add_action(rsp_node)
+    return ld
+
