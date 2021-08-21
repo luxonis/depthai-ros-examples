9984462fb0e6a255bd52a27ec56bb88e27e91d75
Sachin <sachin.guruswamy@gmail.com>
2021-08-20 18:06:14 -0700

    Added Nav2 calls

--- dai_turtlebot3_description/launch/dai_robot.launch.py
+++ dai_turtlebot3_description/launch/dai_robot.launch.py
@@ -1,12 +1,17 @@
-from launch import LaunchDescription, launch_description_sources
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration, Command, ThisLaunchFileDir
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, ThisLaunchFileDir
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
     TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+    
+    bringup_dir = get_package_share_directory('nav2_bringup')
+    launch_dir = os.path.join(bringup_dir, 'launch')
 
     usb_port = LaunchConfiguration('usb_port', default='/dev/ttyACM0')
 
@@ -17,8 +22,75 @@ def generate_launch_description():
             'param',
             TURTLEBOT3_MODEL + '.yaml'))
 
+        # Create the launch configuration variables
+    slam = LaunchConfiguration('slam')
+    namespace = LaunchConfiguration('namespace')
+    use_namespace = LaunchConfiguration('use_namespace')
+    map_yaml_file = LaunchConfiguration('map')
+    params_file = LaunchConfiguration('params_file')
+    default_bt_xml_filename = LaunchConfiguration('default_bt_xml_filename')
+    autostart = LaunchConfiguration('autostart')
+
+    #TODO(Sachin): Do I need to remap this ?
+    """ remappings = [('/tf', 'tf'),
+                  ('/tf_static', 'tf_static')] """
+
+    # Declare the launch arguments
+    declare_namespace_cmd = DeclareLaunchArgument(
+        'namespace',
+        default_value='',
+        description='Top-level namespace')
+
+    declare_use_namespace_cmd = DeclareLaunchArgument(
+        'use_namespace',
+        default_value='false',
+        description='Whether to apply a namespace to the navigation stack')
+
+    declare_slam_cmd = DeclareLaunchArgument(
+        'slam',
+        default_value='False',
+        description='Whether run a SLAM')
+
+    declare_map_yaml_cmd = DeclareLaunchArgument(
+        'map',
+        default_value=os.path.join(bringup_dir, 'maps', 'turtlebot3_world.yaml'),
+        description='Full path to map file to load')
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true')
+
+    declare_params_file_cmd = DeclareLaunchArgument(
+        'params_file',
+        default_value=os.path.join(bringup_dir, 'params', 'nav2_params.yaml'),
+        description='Full path to the ROS2 parameters file to use for all launched nodes')
+
+    declare_bt_xml_cmd = DeclareLaunchArgument(
+        'default_bt_xml_filename',
+        default_value=os.path.join(
+            get_package_share_directory('nav2_bt_navigator'),
+            'behavior_trees', 'follow_point.xml'),
+        description='Full path to the behavior tree xml file to use')
+
+    declare_autostart_cmd = DeclareLaunchArgument(
+        'autostart', default_value='true',
+        description='Automatically startup the nav2 stack')
+
+
+    bringup_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(launch_dir, 'bringup_launch.py')),
+        launch_arguments={'namespace': namespace,
+                          'use_namespace': use_namespace,
+                          'slam': slam,
+                          'map': map_yaml_file,
+                          'use_sim_time': False,
+                          'params_file': params_file,
+                          'default_bt_xml_filename': default_bt_xml_filename,
+                          'autostart': autostart}.items())
+
     turtlebot_state_pub = IncludeLaunchDescription(
-            launch_description_sources.PythonLaunchDescriptionSource(
+            PythonLaunchDescriptionSource(
                 [ThisLaunchFileDir(), '/dai_turtlebot3_state_publisher.launch.py']))
 
     turtlebot_node = Node(
@@ -33,8 +105,10 @@ def generate_launch_description():
             executable='dynamic_tracker',
             output='screen')
 
+
+
     """ turtlebot_rviz = IncludeLaunchDescription(
-            launch_description_sources.PythonLaunchDescriptionSource(
+            PythonLaunchDescriptionSource(
                 [ThisLaunchFileDir(), '/turtlebot_rviz.launch.py'])) """
     """     
     depth_to_scan = Node(
@@ -51,9 +125,20 @@ def generate_launch_description():
 
     ld = LaunchDescription()
 
+    # Declare the launch options
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_use_namespace_cmd)
+    ld.add_action(declare_slam_cmd)
+    ld.add_action(declare_map_yaml_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_params_file_cmd)
+    ld.add_action(declare_bt_xml_cmd)
+    ld.add_action(declare_autostart_cmd)
+
+    ld.add_action(bringup_cmd)
     ld.add_action(turtlebot_state_pub)
     ld.add_action(turtlebot_node)
     ld.add_action(dynamic_tracker)
-#     ld.add_action(turtlebot_rviz) 
+
     return ld
 
