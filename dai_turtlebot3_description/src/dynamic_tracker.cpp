2e28f73f054ea0ee90ceecd72f8bbccc6052249c
Sachin <sachin.guruswamy@gmail.com>
2021-09-14 17:54:28 -0700

    Modified urdf for lite location and changed tracker to stereo only

--- dai_turtlebot3_description/src/dynamic_tracker.cpp
+++ dai_turtlebot3_description/src/dynamic_tracker.cpp
@@ -50,30 +50,32 @@ int main(int argc, char** argv) {
     dai::Pipeline pipeline;
 
     // Define sources and outputs
-    auto camRgb = pipeline.create<dai::node::ColorCamera>();
-    auto spatialDetectionNetwork = pipeline.create<dai::node::MobileNetSpatialDetectionNetwork>();
+    // auto camRgb = pipeline.create<dai::node::ColorCamera>();
+    // auto spatialDetectionNetwork = pipeline.create<dai::node::MobileNetSpatialDetectionNetwork>();
     auto monoLeft = pipeline.create<dai::node::MonoCamera>();
     auto monoRight = pipeline.create<dai::node::MonoCamera>();
     auto stereo = pipeline.create<dai::node::StereoDepth>();
-    auto objectTracker = pipeline.create<dai::node::ObjectTracker>();
+    // auto objectTracker = pipeline.create<dai::node::ObjectTracker>();
 
     auto xoutDepth = pipeline.create<dai::node::XLinkOut>();
     // auto xoutRight = pipeline.create<dai::node::XLinkOut>();
-    auto trackerOut = pipeline.create<dai::node::XLinkOut>();
+    // auto trackerOut = pipeline.create<dai::node::XLinkOut>();
 
     xoutDepth->setStreamName("depth");
-    trackerOut->setStreamName("tracklets");
+    // trackerOut->setStreamName("tracklets");
     // xoutRight->setStreamName("right");
+
     // Properties
-    camRgb->setPreviewSize(300, 300);
-    camRgb->setResolution(dai::ColorCameraProperties::SensorResolution::THE_1080_P);
+    /* camRgb->setPreviewSize(300, 300);
+    camRgb->setResolution(dai::ColorCameraProperties::SensorResolution::THE_12_MP);
     camRgb->setInterleaved(false);
     camRgb->setColorOrder(dai::ColorCameraProperties::ColorOrder::BGR);
     camRgb->setFps(15);
+    */
 
-    monoLeft->setResolution(dai::MonoCameraProperties::SensorResolution::THE_720_P);
+    monoLeft->setResolution(dai::MonoCameraProperties::SensorResolution::THE_480_P);
     monoLeft->setBoardSocket(dai::CameraBoardSocket::LEFT);
-    monoRight->setResolution(dai::MonoCameraProperties::SensorResolution::THE_720_P);
+    monoRight->setResolution(dai::MonoCameraProperties::SensorResolution::THE_480_P);
     monoRight->setBoardSocket(dai::CameraBoardSocket::RIGHT);
     monoLeft->setFps(15);
     monoRight->setFps(15);
@@ -81,28 +83,27 @@ int main(int argc, char** argv) {
     /// setting node configs
     stereo->initialConfig.setConfidenceThreshold(200);
 
-    spatialDetectionNetwork->setBlobPath(nnPath);
+    /* spatialDetectionNetwork->setBlobPath(nnPath);
     spatialDetectionNetwork->setConfidenceThreshold(0.5f);
     spatialDetectionNetwork->input.setBlocking(false);
     spatialDetectionNetwork->setBoundingBoxScaleFactor(0.5);
     spatialDetectionNetwork->setDepthLowerThreshold(100);
-    spatialDetectionNetwork->setDepthUpperThreshold(5000);
+    spatialDetectionNetwork->setDepthUpperThreshold(5000); */
 
-    objectTracker->setDetectionLabelsToTrack({15});  // track only person
+    /* objectTracker->setDetectionLabelsToTrack({15});  // track only person
     // possible tracking types: ZERO_TERM_COLOR_HISTOGRAM, ZERO_TERM_IMAGELESS
     objectTracker->setTrackerType(dai::TrackerType::ZERO_TERM_COLOR_HISTOGRAM);
     // take the smallest ID when new object is tracked, possible options: SMALLEST_ID, UNIQUE_ID
-    objectTracker->setTrackerIdAssigmentPolicy(dai::TrackerIdAssigmentPolicy::SMALLEST_ID);
+    objectTracker->setTrackerIdAssigmentPolicy(dai::TrackerIdAssigmentPolicy::SMALLEST_ID); */
 
     // Linking
     monoLeft->out.link(stereo->left);
     monoRight->out.link(stereo->right);
 
-    camRgb->preview.link(spatialDetectionNetwork->input);
-    // objectTracker->passthroughTrackerFrame.link(xoutRgb->input);
-    objectTracker->out.link(trackerOut->input);
+    /* camRgb->preview.link(spatialDetectionNetwork->input);
+    objectTracker->out.link(trackerOut->input); */
 
-    if(fullFrameTracking) {
+    /* if(fullFrameTracking) {
         camRgb->setPreviewKeepAspectRatio(false);
         camRgb->video.link(objectTracker->inputTrackerFrame);
         objectTracker->inputTrackerFrame.setBlocking(false);
@@ -110,11 +111,11 @@ int main(int argc, char** argv) {
         objectTracker->inputTrackerFrame.setQueueSize(2);
     } else {
         spatialDetectionNetwork->passthrough.link(objectTracker->inputTrackerFrame);
-    }
+    } */
 
-    spatialDetectionNetwork->passthrough.link(objectTracker->inputDetectionFrame);
+    /* spatialDetectionNetwork->passthrough.link(objectTracker->inputDetectionFrame);
     spatialDetectionNetwork->out.link(objectTracker->inputDetections);
-    stereo->depth.link(spatialDetectionNetwork->inputDepth);
+    stereo->depth.link(spatialDetectionNetwork->inputDepth); */
     stereo->depth.link(xoutDepth->input);
     // stereo->syncedRight.link(xoutRight->input);
     // Connect to device and start pipeline
@@ -122,18 +123,18 @@ int main(int argc, char** argv) {
 
     auto depthQueue = device.getOutputQueue("depth", 4, false);
     // auto rightQueue = device.getOutputQueue("right", 4, false);
-    auto trackletsQueue = device.getOutputQueue("tracklets", 4, false);
+    // auto trackletsQueue = device.getOutputQueue("tracklets", 4, false);
 
     std::string camera_param_uri = "package://depthai_examples/params/camera";
     std::string stereo_uri = camera_param_uri + "/" + "right.yaml";
     
-    dai::rosBridge::BridgePublisher<geometry_msgs::msg::PoseStamped, dai::Tracklets> trackerPublish(trackletsQueue,
+    /* dai::rosBridge::BridgePublisher<geometry_msgs::msg::PoseStamped, dai::Tracklets> trackerPublish(trackletsQueue,
                                                                                                     node, 
                                                                                                     std::string("goal_update"),
                                                                                                     toRosMsg, 
                                                                                                     rclcpp::QoS(rclcpp::KeepLast(10)));
 
-    trackerPublish.addPubisherCallback();
+    trackerPublish.addPubisherCallback(); */
 
     dai::rosBridge::ImageConverter depthConverter("oak-d_right_camera_optical_frame", true);
     dai::rosBridge::BridgePublisher<sensor_msgs::msg::Image, dai::ImgFrame> depthPublish(depthQueue,
