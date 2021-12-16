00ec292af759fe43e466a029b7c84abab7edc9bc
Sachin Guruswamy <43363595+saching13@users.noreply.github.com>
2021-12-15 18:23:07 -0800

    Update launch files (#17)
    * Add more arguments options to connect and modify urdf's TF tree
    
    * Updated launch files
    
    * Clean up and style launch files
    
    * Updated styling

--- depthai_examples/launch/mobile_publisher.launch.py
+++ depthai_examples/launch/mobile_publisher.launch.py
@@ -14,23 +14,71 @@ def generate_launch_description():
                                 'rviz', 'pointCloud.rviz')
     urdf_launch_dir = os.path.join(get_package_share_directory('depthai_bridge'), 'launch')
     
+    camera_model     = LaunchConfiguration('camera_model',  default = 'OAK-D')
+    camera_name      = LaunchConfiguration('camera_name',   default = 'oak')
+    base_frame       = LaunchConfiguration('base_frame',    default = 'oak-d_frame')
+    parent_frame     = LaunchConfiguration('parent_frame',  default = 'oak-d-base-frame')
+    cam_pos_x        = LaunchConfiguration('cam_pos_x',     default = '0.0')
+    cam_pos_y        = LaunchConfiguration('cam_pos_y',     default = '0.0')
+    cam_pos_z        = LaunchConfiguration('cam_pos_z',     default = '0.0')
+    cam_roll         = LaunchConfiguration('cam_roll',      default = '0.0')
+    cam_pitch        = LaunchConfiguration('cam_pitch',     default = '0.0')
+    cam_yaw          = LaunchConfiguration('cam_yaw',       default = '0.0')
 
-    camera_name      = LaunchConfiguration('camera_name', default = 'oak')
-    camera_model     = LaunchConfiguration('camera_model', default = 'OAK-D')
     camera_param_uri = LaunchConfiguration('camera_param_uri', default = 'package://depthai_examples/params/camera')
     sync_nn          = LaunchConfiguration('sync_nn', default = True)
     nn_path          = LaunchConfiguration('nn_path', default = "")
 
 
-    declare_camera_name_cmd = DeclareLaunchArgument(
-        'camera_name',
-        default_value=camera_name,
-        description='The name of the camera. It can be different from the camera model and it will be used in naming TF.')
-    
     declare_camera_model_cmd = DeclareLaunchArgument(
         'camera_model',
         default_value=camera_model,
         description='The model of the camera. Using a wrong camera model can disable camera features. Valid models: `OAK-D, OAK-D-LITE`.')
+
+    declare_camera_name_cmd = DeclareLaunchArgument(
+        'camera_name',
+        default_value=camera_name,
+        description='The name of the camera. It can be different from the camera model and it will be used in naming TF.')
+
+    declare_base_frame_cmd = DeclareLaunchArgument(
+        'base_frame',
+        default_value=base_frame,
+        description='Name of the base link.')
+
+    declare_parent_frame_cmd = DeclareLaunchArgument(
+        'parent_frame',
+        default_value=parent_frame,
+        description='Name of the parent link from other a robot TF for example that can be connected to the base of the OAK.')
+
+    declare_pos_x_cmd = DeclareLaunchArgument(
+        'cam_pos_x',
+        default_value=cam_pos_x,
+        description='Position X of the camera with respect to the base frame.')
+
+    declare_pos_y_cmd = DeclareLaunchArgument(
+        'cam_pos_y',
+        default_value=cam_pos_y,
+        description='Position Y of the camera with respect to the base frame.')
+
+    declare_pos_z_cmd = DeclareLaunchArgument(
+        'cam_pos_z',
+        default_value=cam_pos_z,
+        description='Position Z of the camera with respect to the base frame.')
+
+    declare_roll_cmd = DeclareLaunchArgument(
+        'cam_roll',
+        default_value=cam_roll,
+        description='Roll orientation of the camera with respect to the base frame.')
+
+    declare_pitch_cmd = DeclareLaunchArgument(
+        'cam_pitch',
+        default_value=cam_pitch,
+        description='Pitch orientation of the camera with respect to the base frame.')
+
+    declare_yaw_cmd = DeclareLaunchArgument(
+        'cam_yaw',
+        default_value=cam_yaw,
+        description='Yaw orientation of the camera with respect to the base frame.')
 
     declare_camera_param_uri_cmd = DeclareLaunchArgument(
         'camera_param_uri',
@@ -50,9 +98,16 @@ def generate_launch_description():
     urdf_launch = IncludeLaunchDescription(
                             launch_description_sources.PythonLaunchDescriptionSource(
                                     os.path.join(urdf_launch_dir, 'urdf_launch.py')),
-                            launch_arguments={'camera_name': camera_name,
-                                              'camera_model': camera_model}.items())
-
+                            launch_arguments={'camera_name' : camera_name,
+                                              'camera_model': camera_model,
+                                              'base_frame'  : base_frame,
+                                              'parent_frame': parent_frame,
+                                              'cam_pos_x'   : cam_pos_x,
+                                              'cam_pos_y'   : cam_pos_y,
+                                              'cam_pos_z'   : cam_pos_z,
+                                              'cam_roll'    : cam_roll,
+                                              'cam_pitch'   : cam_pitch,
+                                              'cam_yaw'     : cam_yaw}.items())
 
     mobilenet_node = launch_ros.actions.Node(
             package='depthai_examples', executable='mobilenet_node',
@@ -69,6 +124,17 @@ def generate_launch_description():
     ld = LaunchDescription()
     ld.add_action(declare_camera_name_cmd)
     ld.add_action(declare_camera_model_cmd)
+    
+    ld.add_action(declare_base_frame_cmd)
+    ld.add_action(declare_parent_frame_cmd)
+    
+    ld.add_action(declare_pos_x_cmd)
+    ld.add_action(declare_pos_y_cmd)
+    ld.add_action(declare_pos_z_cmd)
+    ld.add_action(declare_roll_cmd)
+    ld.add_action(declare_pitch_cmd)
+    ld.add_action(declare_yaw_cmd)
+
     ld.add_action(declare_camera_param_uri_cmd)
     ld.add_action(declare_sync_nn_cmd)
     ld.add_action(declare_nn_path_cmd)
