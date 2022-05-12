922548cef77d91bba7b20ebc19fa725866104fd6
Sachin <sachin.guruswamy@gmail.com>
2022-05-12 15:36:49 -0700

    Updated with node

--- depthai_examples/ros1_scripts/markerPublisher.py
+++ depthai_examples/ros1_scripts/markerPublisher.py
@@ -1,7 +1,7 @@
 import rospy
 from foxglove_msgs.msg import ImageMarkerArray
 from visualization_msgs.msg import ImageMarker
-from depthai_ros_msgs.msg import SpatialImgDetections
+from depthai_ros_msgs.msg import SpatialDetectionArray
 from geometry_msgs.msg import Point
 from std_msgs.msg import ColorRGBA
 
@@ -10,10 +10,10 @@ from std_msgs.msg import ColorRGBA
 class SpatialDetectionsVisualizationNode:
     def __init__(self):
        rospy.init_node("my_node")
-       rospy.Subscriber("/stereo_inertial_publisher/color/yolov4_Spatial_detections", SpatialImgDetections, self.detectionCallback, queue_size=1)
+       rospy.Subscriber("spatialDetections", SpatialDetectionArray, self.detectionCallback, queue_size=1)
 
        # On initialization, set up a Publisher for ImageMarkerArrays
-       self.pub_markers = rospy.Publisher("/spatialDetectionMarkers", ImageMarkerArray, queue_size=1)
+       self.pub_markers = rospy.Publisher("spatialDetectionMarkers", ImageMarkerArray, queue_size=1)
        
        rospy.spin()
 
