fb22ec5c0673fed83d973a5cbe637255216e06ae
Sachin <sachin.guruswamy@gmail.com>
2022-08-11 09:58:35 -0700

    Upgrades to undistort omni

--- depthai_examples/ros1_scripts/undistort_omni.py
+++ depthai_examples/ros1_scripts/undistort_omni.py
@@ -3,6 +3,8 @@ import rospy
 import cv2
 import numpy as np
 from cv_bridge import CvBridge
+import yaml
+
 # from depthai_ros_msgs.msg import SpatialDetectionArray
 from foxglove_msgs.msg import ImageMarkerArray
 from visualization_msgs.msg import ImageMarker
@@ -28,7 +30,31 @@ class OmniUndistort:
             disp12MaxDiff=LRthreshold,
             mode=cv2.STEREO_SGBM_MODE_SGBM_3WAY
         )
- 
+        
+        with open('/home/sachin/kalibr_bags/12_k_res/aug_data/2022-08-02-13-20-02-camchain.yaml') as file:
+            camChainDict = yaml.load(file, Loader=yaml.FullLoader)
+            # print(camChainDict)
+
+        intrinsic = camChainDict['cam0']['intrinsics']
+        self.xiLeft = np.array([[intrinsic[0]]], float)
+        self.kLeft = np.array([[intrinsic[1], 0, intrinsic[3]], 
+                               [0, intrinsic[2], intrinsic[4]], 
+                               [0, 0, 1.0]], float)
+        
+        print(camChainDict['cam0']['distortion_coeffs'])
+        self.dLeft = np.array(camChainDict['cam0']['distortion_coeffs'], float)
+        # dLeft  = np.array([[-0.12250034916583766, 0.5409617731001941, -0.0027348679896492937, -0.0033019856319997736]], float)
+
+        intrinsic = camChainDict['cam1']['intrinsics']
+        self.xiRight = np.array([[intrinsic[0]]], float)
+        self.kRight = np.array([[intrinsic[1], 0, intrinsic[3]], 
+                               [0, intrinsic[2], intrinsic[4]], 
+                               [0, 0, 1.0]], float)
+        self.dRight = np.array(camChainDict['cam1']['distortion_coeffs'], float)
+
+        transformation = np.array(camChainDict['cam1']['T_cn_cnm1'], float)
+        self.R = transformation[:3, :3]
+        self.t = transformation[:3, 3]
 
         left_sub  = message_filters.Subscriber("/stereo_inertial_publisher/left/image_raw",  Image)
         right_sub = message_filters.Subscriber("/stereo_inertial_publisher/right/image_raw", Image)
@@ -129,68 +155,56 @@ class OmniUndistort:
         return undistortedPerspectiveMsg, undistortedCylindricalMsg, undistortedLongLatiMsg, undistortedStereographicMsg
 
     def stereoReconstructCallback(self, leftImgMsg, rightImageMsg):
+        print("Printing Left message and right msg timestamps.." )
+        print(leftImgMsg.header.stamp)
+        print(rightImageMsg.header.stamp)
+        
+        
+        # kLeft  = np.array([[755.220194243529, 0, 503.7627638065912], 
+        #                    [0, 752.5595083167417, 379.12063549278554], 
+        #                    [0, 0, 1.0]], float)
+        # dLeft  = np.array([[-0.12250034916583766, 0.5409617731001941, -0.0027348679896492937, -0.0033019856319997736]], float)
+        # xiLeft = np.array([[2.0554874923292057]], float)
 
-        kLeft  = np.array([[755.220194243529, 0, 503.7627638065912], 
-                           [0, 752.5595083167417, 379.12063549278554], 
-                           [0, 0, 1.0]], float)
-        dLeft  = np.array([[-0.12250034916583766, 0.5409617731001941, -0.0027348679896492937, -0.0033019856319997736]], float)
-        xiLeft = np.array([[2.0554874923292057]], float)
+        # kRight  = np.array([[757.1337928004853, 0, 508.14972031711176],
+        #                     [0, 754.3808116028206, 377.7936644736236],
+        #                     [0, 0, 1]], float)
 
-        kRight  = np.array([[757.1337928004853, 0, 508.14972031711176],
-                            [0, 754.3808116028206, 377.7936644736236],
-                            [0, 0, 1]], float)
+        # dRight  = np.array([[-0.12466843249136443, 0.4539994383192077, -0.0020298550160945754, -0.002938034843103457]], float)
+        # xiRight = np.array([[2.041547658038243]], float)
 
-        dRight  = np.array([[-0.12466843249136443, 0.4539994383192077, -0.0020298550160945754, -0.002938034843103457]], float)
-        xiRight = np.array([[2.041547658038243]], float)
-
-        # transformation = np.array([[0.9999378975984609, -0.004352271870197581, -0.010259565094392187, 0.04263875562328708],
-        #                            [0.004565082250721026, 0.9997729992246492, 0.020811296100477848, 0.000654833800297008],
-        #                            [0.010166659746560596, -0.02085683942752352, 0.9997307793994743, 0.0003253521964047307]], float)
+        # # transformation = np.array([[0.9999378975984609, -0.004352271870197581, -0.010259565094392187, 0.04263875562328708],
+        # #                            [0.004565082250721026, 0.9997729992246492, 0.020811296100477848, 0.000654833800297008],
+        # #                            [0.010166659746560596, -0.02085683942752352, 0.9997307793994743, 0.0003253521964047307]], float)
 
 
-        transformation = np.array([[0.999986523390199, 0.004976222165131139, 0.0014799496434515056, -0.04260840945432192],
-                                   [-0.0049631869672225526, 0.9999499689519986, -0.008684835520975933, -0.00013985148542148193],
-                                   [-0.0015230932710398521, 0.008677373212053903, 0.9999611909374421, -0.00016307543333419315],
-                                   [0.0, 0.0, 0.0, 1.0]], float)
+        # transformation = np.array([[0.999986523390199, 0.004976222165131139, 0.0014799496434515056, -0.04260840945432192],
+        #                            [-0.0049631869672225526, 0.9999499689519986, -0.008684835520975933, -0.00013985148542148193],
+        #                            [-0.0015230932710398521, 0.008677373212053903, 0.9999611909374421, -0.00016307543333419315],
+        #                            [0.0, 0.0, 0.0, 1.0]], float)
 
-        R = transformation[:3, :3]
+        # R = transformation[:3, :3]
 
         from scipy.spatial.transform import Rotation
-        print('R----->')
-        print(R)
-        rot = Rotation.from_matrix(R)
-        print(rot.as_euler('xyz', degrees=True))
+        # print('R----->')
+        # print(R)
+        # rot = Rotation.from_matrix(R)
+        # print(rot.as_euler('xyz', degrees=True))
 
-
-        # R = np.array([[-0.999853,    0.0153559,  -0.00762841],
-        #               [-0.0153555,    -0.999882, -0.000117155],
-        #               [-0.00762931,   0.0,     0.999971]], float)
-        """ 
-        R = R.transpose()
-
-        print('R New---- ->')
-        print(R)
-        rot = Rotation.from_matrix(R)
-        print(rot.as_euler('xyz', degrees=True))
-        """
-        
-        # t = - np.dot(R, transformation[:, 3])
-        t = transformation[:3, 3]
-        # print("Prinyiny Org T")
-        # print(transformation[:, 3])
-        print("Printing T")
-        print(t)
+        # print("Printing T")
+        # print(t)
 
         flag = cv2.omnidir.RECTIFY_PERSPECTIVE
         bridge = CvBridge()
         imageLeft  = bridge.imgmsg_to_cv2(leftImgMsg, desired_encoding='passthrough')
         bridge = CvBridge()
         imageRight = bridge.imgmsg_to_cv2(rightImageMsg, desired_encoding='passthrough')
-        numDisparities = 16
-        SADWindowSize = 3
+        
 
-        if 1: # stereo mode with direct reconstruction. Not working. there seems to be offset. 
-            disp, imgLeftRect, imgRightRect, pointCloud = cv2.omnidir.stereoReconstruct(imageLeft, imageRight, kLeft, dLeft, xiLeft, kRight, dRight, xiRight, R, t, flag, numDisparities, SADWindowSize)
+        if 0: # stereo mode with direct reconstruction. Not working. there seems to be offset. 
+            numDisparities = 16
+            SADWindowSize = 3
+            disp, imgLeftRect, imgRightRect, pointCloud = cv2.omnidir.stereoReconstruct(imageLeft, imageRight, self.kLeft, self.dLeft, self.xiLeft, self.kRight, self.dRight, self.xiRight, self.R, self.t, flag, numDisparities, SADWindowSize)
 
             cv2.imshow("disp", disp)
             cv2.imshow("imgLeftRect", imgLeftRect)
@@ -198,8 +212,7 @@ class OmniUndistort:
             cv2.waitKey(1)
         # undistortedPerspective = cv2.omnidir.undistortImage(image, k, d, xi, cv2.omnidir.RECTIFY_PERSPECTIVE, R = rot)
         if 0:
-            R1, R2 = cv2.omnidir.stereoRectify(R, t)
-            
+            R1, R2 = cv2.omnidir.stereoRectify(self.R, self.t)
             print('R1----->')
             print(R1)
             rot = Rotation.from_matrix(R1)
@@ -209,27 +222,39 @@ class OmniUndistort:
             rot = Rotation.from_matrix(R2)
             print(rot.as_euler('xyz', degrees=True))
 
-            undistortedPerspectiveLeft = cv2.omnidir.undistortImage(imageLeft, kLeft, dLeft, xiLeft, cv2.omnidir.RECTIFY_PERSPECTIVE     , R = R1)
-            undistortedPerspectiveRight = cv2.omnidir.undistortImage(imageRight, kRight, dRight, xiRight, cv2.omnidir.RECTIFY_PERSPECTIVE, R = R2)
+            undistortedPerspectiveLeft = cv2.omnidir.undistortImage(imageLeft,   self.kLeft, self.dLeft, self.xiLeft, cv2.omnidir.RECTIFY_PERSPECTIVE     , R = R1)
+            undistortedPerspectiveRight = cv2.omnidir.undistortImage(imageRight, self.kRight, self.dRight, self.xiRight, cv2.omnidir.RECTIFY_PERSPECTIVE, R = R2)
 
             cv2.imshow("imageRight", imageRight)
             cv2.imshow("undistortedPerspectiveLeft", undistortedPerspectiveLeft)
             cv2.imshow("undistortedPerspectiveRight", undistortedPerspectiveRight)
             cv2.waitKey(1)
-        if 0:
-            R1, R2 = cv2.omnidir.stereoRectify(R, t)
-            print('R1----->')
-            print(R1)
-            rot = Rotation.from_matrix(R1)
-            print(rot.as_euler('xyz', degrees=True))
-            print('R2----->')
-            print(R2)
-            rot = Rotation.from_matrix(R2)
-            print(rot.as_euler('xyz', degrees=True))
 
-            undistortedPerspectiveLeft = cv2.omnidir.undistortImage(imageLeft, kLeft, dLeft, xiLeft, cv2.omnidir.RECTIFY_PERSPECTIVE     , R = R1)
-            undistortedPerspectiveRight = cv2.omnidir.undistortImage(imageRight, kRight, dRight, xiRight, cv2.omnidir.RECTIFY_PERSPECTIVE, R = R2)
+        if 1:
+            R1, R2 = cv2.omnidir.stereoRectify(self.R, self.t)
+            # -60 deg Y axis rotation
+            rot = np.array([[0.5000000,  0.0000000, -0.8660254],
+                        [0.0000000,  1.0000000,  0.0000000],
+                        [0.8660254,  0.0000000,  0.5000000]])
 
+            # rot = np.array([[0.5000000,  0.0000000, -0.8660254],
+            #             [0.2961981,  0.9396926,  0.1710101],
+            #             [0.8137977, -0.3420202,  0.4698463]])
+
+            R1 = np.matmul(R1, rot)
+            R2 = np.matmul(R2, rot)            
+
+            # print('R1----->')
+            # print(R1)
+            # rot = Rotation.from_matrix(R1)
+            # print(rot.as_euler('xyz', degrees=True))
+            # print('R2----->')
+            # print(R2)
+            # rot = Rotation.from_matrix(R2)
+            # print(rot.as_euler('xyz', degrees=True))
+
+            undistortedPerspectiveLeft = cv2.omnidir.undistortImage(imageLeft,   self.kLeft, self.dLeft, self.xiLeft, cv2.omnidir.RECTIFY_PERSPECTIVE, R = R1)
+            undistortedPerspectiveRight = cv2.omnidir.undistortImage(imageRight, self.kRight, self.dRight, self.xiRight, cv2.omnidir.RECTIFY_PERSPECTIVE, R = R2)
 
             subpixelBits = 16.
             disparity = self.stereoProcessor.compute(undistortedPerspectiveLeft, undistortedPerspectiveRight)
@@ -239,7 +264,7 @@ class OmniUndistort:
             cv2.imshow("undistortedPerspectiveRight", undistortedPerspectiveRight)
             cv2.imshow("disparity", disparity)
             key = cv2.waitKey(1)
-
+        print('Exiting Callback')
 
 
   
